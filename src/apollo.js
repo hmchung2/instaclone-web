9707a24c3a9f79a3542cccba6061a216c28ed911
hmchung2 <hmchung2@gmail.com>
2023-02-05 05:49:23 +0900

    dark mode

--- src/apollo.js
+++ src/apollo.js
@@ -1,10 +1,9 @@
 import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
 
-const TOKEN = "token";
+const TOKEN = "TOKEN";
+const DARK_MODE = "DARK_MODE";
 
-export const isLoggedInVar = makeVar(
-  Boolean(localStorage.getItem(TOKEN)) ? true : false
-);
+export const isLoggedInVar = makeVar(Boolean(localStorage.getItem(TOKEN)));
 
 export const logUserIn = (token) => {
   localStorage.setItem(TOKEN, token);
@@ -13,12 +12,23 @@ export const logUserIn = (token) => {
 
 export const logUserOut = () => {
   localStorage.removeItem(TOKEN);
-  //   isLoggedInVar(false);
-  window.location.reload();
+  window.location.reload(true);
+  window.history.forward(1);
 };
 
-export const darkModeVar = makeVar(false);
+export const darkModeVar = makeVar(Boolean(localStorage.getItem(DARK_MODE)));
+
+export const enableDarkMode = () => {
+  localStorage.setItem(DARK_MODE, "enabled");
+  darkModeVar(true);
+};
+
+export const disableDarkMode = () => {
+  localStorage.removeItem(DARK_MODE);
+  darkModeVar(false);
+};
+
 export const client = new ApolloClient({
-  uri: "http://127.0.0.1:4000/graphql",
+  uri: "http://localhost:4000/graphql",
   cache: new InMemoryCache(),
 });
