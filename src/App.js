4da5ac8dbabc9251fc65c39e89f3ef948c36b9e5
hmchung2 <hmchung2@gmail.com>
2023-02-04 17:32:44 +0900

    reactive var

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
+import { useReactiveVar } from "@apollo/client";
+import { useState } from "react";
 import {
   HashRouter as Router,
   Route,
   Switch,
   Redirect,
 } from "react-router-dom";
+import { isLoggedInVar } from "./apollo";
+
 import Home from "./screens/Home";
 import Login from "./screens/Login";
 import NotFound from "./screens/NotFound";
 
 function App() {
-  const isLoggedIn = false;
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
 
   return (
     <div>
